04984ff7215dd906969e87492c169310361709fd
Ryan Davis <void@rdavis.xyz>
2017-01-06 14:55:03 +1100

    Dismiss activity when navigating back from markdown viewer

--- app/src/main/java/xyz/devspace/md/MarkdownViewerActivity.java
+++ app/src/main/java/xyz/devspace/md/MarkdownViewerActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MarkdownViewerActivity extends AppCompatActivity {
@@ -31,4 +32,12 @@ public class MarkdownViewerActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
