967b81313735d4cc593c4306d9131e05a6b803d9
Ryan Davis <void@rdavis.xyz>
2017-01-07 10:20:13 +1100

    Use ACTION_GET_CONTENT intent when selecting files
    This intent is recommended over the ACTION_OPEN_DOCUMENT intent for
    cases where the file is not intended to be opened for a prolonged period
    of time.

--- app/src/main/java/com/braindump/md/MainActivity.java
+++ app/src/main/java/com/braindump/md/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
                 intent.setType("application/*");
                 startActivityForResult(intent, READ_REQUEST_CODE);
