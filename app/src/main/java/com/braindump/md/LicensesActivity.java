4f2617c4d9400140e05de23661ba8b6b4c50cdf9
Ryan Davis <void@rdavis.xyz>
2017-01-07 13:50:39 +1100

    Re-enable up navigation from the licenses activity

--- app/src/main/java/com/braindump/md/LicensesActivity.java
+++ app/src/main/java/com/braindump/md/LicensesActivity.java
@@ -1,9 +1,11 @@
 package com.braindump.md;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.webkit.WebView;
 
 public class LicensesActivity extends AppCompatActivity {
@@ -15,8 +17,21 @@ public class LicensesActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         WebView webView = (WebView) findViewById(R.id.license_web_view);
         webView.loadUrl("file:///android_asset/licenses.html");
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
