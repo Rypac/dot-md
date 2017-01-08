db42deedeb5e5e32fa77acf0a04d0be7ec51a1a6
Ryan Davis <void@rdavis.xyz>
2017-01-08 17:32:00 +1100

    Convert license activity to anko

--- app/src/main/java/com/braindump/md/LicensesActivity.kt
+++ app/src/main/java/com/braindump/md/LicensesActivity.kt
@@ -1,24 +1,52 @@
 package com.braindump.md
 
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
+import android.support.design.widget.AppBarLayout.LayoutParams.*
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.view.MenuItem
-import android.webkit.WebView
+import android.view.ViewManager
+import org.jetbrains.anko.*
+import org.jetbrains.anko.appcompat.v7.titleResource
+import org.jetbrains.anko.appcompat.v7.toolbar
+import org.jetbrains.anko.custom.ankoView
+import org.jetbrains.anko.design.appBarLayout
+import org.jetbrains.anko.design.coordinatorLayout
+import org.jetbrains.anko.support.v4.nestedScrollView
+import org.sufficientlysecure.htmltextview.HtmlTextView
 
 class LicensesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_licenses)
 
-        with(findViewById(R.id.toolbar) as Toolbar) {
-            setSupportActionBar(this)
+        coordinatorLayout {
+            fitsSystemWindows = true
+            appBarLayout(theme = R.style.AppTheme_AppBarOverlay) {
+                toolbar {
+                    id = ID_TOOLBAR
+                    titleResource = R.string.title_licenses
+                    popupTheme = R.style.AppTheme_PopupOverlay
+                }.lparams(width = matchParent, height = wrapContent) {
+                    scrollFlags = SCROLL_FLAG_SCROLL.or(SCROLL_FLAG_ENTER_ALWAYS)
+                }
+            }.lparams(width = matchParent, height = wrapContent)
+            nestedScrollView {
+                htmlTextView {
+                    id = ID_LICENSES
+                    verticalPadding = dimen(R.dimen.activity_vertical_margin)
+                    horizontalPadding = dimen(R.dimen.activity_horizontal_margin)
+                }
+            }.lparams(width = matchParent, height = matchParent) {
+                behavior = AppBarLayout.ScrollingViewBehavior()
+            }
         }
+
+        setSupportActionBar(find(ID_TOOLBAR))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        with(findViewById(R.id.license_web_view) as WebView) {
-            loadUrl("file:///android_asset/licenses.html")
+        with(find<HtmlTextView>(ID_LICENSES)) {
+            setHtml(R.raw.licenses)
         }
     }
 
@@ -30,4 +58,12 @@ class LicensesActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+
+    companion object {
+        private val ID_TOOLBAR = 1000
+        private val ID_LICENSES = 1001
+    }
+
+    inline fun ViewManager.htmlTextView(theme: Int = 0, init: HtmlTextView.() -> Unit) =
+        ankoView(::HtmlTextView, theme, init)
 }
